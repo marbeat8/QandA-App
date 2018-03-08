e5f80d681c47ae8dccc0a607126194b058e6d139
cr0nas <stolz.jonas@gmail.com>
2018-03-08 15:26:15 +0100

    Get Question from DB

--- app/src/main/java/com/example/marcusmller/qa_app/MainActivity.java
+++ app/src/main/java/com/example/marcusmller/qa_app/MainActivity.java
@@ -10,8 +10,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.ConsoleMessage;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +37,28 @@ public class MainActivity extends AppCompatActivity   {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        //Fragen aus Datenbank laden
+        getQuestionFromDB();
     }
+
+    //Fragen aus DB laden
+    private void getQuestionFromDB(){
+        FragenAusDatenbank dbAbfrage = new FragenAusDatenbank();
+        String dbResponse = "";
+        dbResponse = dbAbfrage.doInBackground();
+        ArrayList<String> arrListFrage,arrListFragenUser = new ArrayList<String>();
+        arrListFragenUser = dbAbfrage.jsonToArrList(dbResponse,"User");
+        arrListFrage = dbAbfrage.jsonToArrList(dbResponse,"Frage");
+        Log.d("Test: ",""+arrListFrage.size());
+        for(String str : arrListFragenUser){
+            Log.d("Test: ",str);
+        }
+        for(int i =0;i<arrListFrage.size(); i++){
+            //Frage aus db in Liste schreiben
+            FragmentOne.list.add(arrListFragenUser.get(i)+" am DatumFehltNoch:\n"+arrListFrage.get(i));
+        }
+    }
+
     // Adapter for the viewpager using FragmentPagerAdapter
     class ViewPagerAdapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
