6bb9da8579df396c91368826bfd7b1f5ca621899
marbeat8 <s_muellermarcus@stud.hwr-berlin.de>
2018-03-08 15:44:34 +0100

    add Toolbar add Listview add TabNavigation (FragmentOne FragmentTwo) add function "Nützliche Links" (FragementTwo and AddLink.java)

--- app/src/main/java/com/example/marcusmller/qa_app/MainActivity.java
+++ app/src/main/java/com/example/marcusmller/qa_app/MainActivity.java
@@ -9,8 +9,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
@@ -66,12 +68,30 @@ public class MainActivity extends AppCompatActivity   {
     //Toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        MenuItem item =menu.findItem(R.id.search);
+        SearchView searchView =(SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                FragmentOne.adapter.getFilter().filter(newText);
+                FragmentTwo.adapter.getFilter().filter(newText);
+                return false;
+            }
+
+        });
+
+
+
+        return super.onCreateOptionsMenu(menu);
     }
-
-
 
 
 
